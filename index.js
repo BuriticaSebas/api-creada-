aca86f7af6254308e22b9e3e0b4227f5b7d2620e
BuriticaSebas <jhoseburitica40@gmail.com>
2024-08-13 16:19:10 -0500

    Sebastian

--- index.js
+++ index.js
@@ -6,91 +6,63 @@ const port = process.env.PORT || 3000;
 const cors = require('cors');
 app.use(cors());
 
-
-const students = [
+const sebastian = [
     {
-        personajesDoblados:{
-        rickI: "Justin Roiland",
-        mortyI: "Justin Roiland",
-        rickE: "Juan Guzman",
-        mortyE: "Eder la Barrera",
+        personajesDoblados: {
+            rickI: "Justin Roiland",
+            mortyI: "Justin Roiland",
+            rickE: "Juan Guzman",
+            mortyE: "Eder la Barrera",
         },
-
         temporadas: {
-            1:{ 
-                episodios:11,
-                primeraE:"2 de Diciembre de 2013",
-                ultimaE:"14 de Abri de 2014",
+            1: { 
+                episodios: 11,
+                primeraE: "2 de Diciembre de 2013",
+                ultimaE: "14 de Abri de 2014",
             },
-
             2: {
-                episodios:10,
-                primeraE:"26 de julio de 2015",
-                ultimaE:"1 de Abri de 2017",},
-
+                episodios: 10,
+                primeraE: "26 de julio de 2015",
+                ultimaE: "1 de Abri de 2017",
+            },
             3: {
-                episodios:10,
-                primeraE:"1 de Abril de 2017",
-                ultimaE:"1 de octubre de 2017",
+                episodios: 10,
+                primeraE: "1 de Abril de 2017",
+                ultimaE: "1 de octubre de 2017",
             },
-
             4: {
-                episodios:10,
-                primeraE:"10 de Noviembre de 2019",
-                ultimaE:"31 de octubre de 2020",
+                episodios: 10,
+                primeraE: "10 de Noviembre de 2019",
+                ultimaE: "31 de octubre de 2020",
             },
-
             5: {
-                episodios:10,
-                primeraE:"20 de Junio de 2021",
-                ultimaE:"5 de septiembre de 2021",
+                episodios: 10,
+                primeraE: "20 de Junio de 2021",
+                ultimaE: "5 de septiembre de 2021",
             },
-
             6: {
-                episodios:10,
-                primeraE:"4 de Septiembre de 2022",
-                ultimaE:"11 de Diciembre de 2022",
+                episodios: 10,
+                primeraE: "4 de Septiembre de 2022",
+                ultimaE: "11 de Diciembre de 2022",
             },
-
             7: {
-                episodios:10,
-                primeraE:"15 de Octubre de 2023",
-                ultimaE:"17 de Diciembre de 2023",
+                episodios: 10,
+                primeraE: "15 de Octubre de 2023",
+                ultimaE: "17 de Diciembre de 2023",
             }
-
         },
-    
     },
 ];
-
-
-
-
 
 app.get("/", (req, res) => {    
     res.send('Node JS api');
 });
 
-app.get("/api/students/", (req, res) => {
-    res.send(students);
+app.get("/api/sebastian/", (req, res) => {
+    res.send(sebastian);
 });
-
-
-app.get("/api/students/:id", (req, res) => {
-    const student = students.find(s => s.id == parseInt(req.params.id));
-    if (!student) {
-        res.status(404).send("Student not found");
-    }else{
-        res.send(student);
-    }
-});
-
-
 
 
 app.listen(port, () => {
-  console.log(`port runing in http://localhost:${port}`);
+  console.log(`port running on http://localhost:${port}`);
 });
-
-
-
