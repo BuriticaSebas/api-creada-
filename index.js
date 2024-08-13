63f41fdd4516f5d58ea227b4090916081b39fcb3
BuriticaSebas <jhoseburitica40@gmail.com>
2024-08-13 15:01:57 -0500

    melo

--- index.js
+++ index.js
@@ -1,53 +1,22 @@
 const express = require("express");
 const app = express();
 
-app.use(express.json());
-
-const students = [
-    { id: 1, name: "John", age: 20 },
-    { id: 2, name: "Jane", age: 21 },
-    { id: 3, name: "Joe", age: 22 },
-];
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send('Node JS API');
+  const htmlResponse = `
+    <html>
+      <head>
+        <title>NodeJs y Express en Vercel</title>
+      </head>
+      <body>
+        <h1>Soy un proyecto Back end en vercel</h1>
+      </body>
+    </html>
+  `;
+  res.send(htmlResponse);
 });
 
-app.get("/api/students", (req, res) => {
-    res.send(students);
+app.listen(port, () => {
+  console.log(`port runing in http://localhost:${port}`);
 });
-
-app.get("/api/students/:id", (req, res) => {
-    const student = students.find(s => s.id == parseInt(req.params.id));
-    if (!student) {
-        res.status(404).send("Student not found");
-    } else {
-        res.send(student);
-    }
-});
-
-app.post("/api/students", (req, res) => {
-    const student = {
-        id: students.length + 1,
-        name: req.body.name,
-        age: parseInt(req.body.age),
-        enroll: (req.body.enroll === true)
-    };
-    students.push(student);
-    res.send(student);
-});
-
-app.delete("/api/students/:id", (req, res) => {
-    const student = students.find(s => s.id == parseInt(req.params.id));
-    if (!student) {
-        res.status(404).send("Student not found");
-    } else {
-        const index = students.indexOf(student);
-        students.splice(index, 1);
-        res.send(student);
-    }
-});
-
-module.exports = app;
-
-
