471d55488d09d3f75fefa5569ee3d2e1824b9375
BuriticaSebas <jhoseburitica40@gmail.com>
2024-08-13 15:14:12 -0500

    prueba2

--- index.js
+++ index.js
@@ -3,9 +3,36 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+
+const students = [
+    {
+
+        id: 1,
+        name: "John",
+        age: 20
+    },
+    {
+        id: 2,
+        name: "Jane",
+        age: 21
+    },
+    {
+        id: 3,
+        name: "Joe",
+        age: 22
+    }
+
+];
+
 app.get("/", (req, res) => {    
     res.send('Node JS api');
 });
+
+app.get("/api/students/", (req, res) => {
+    res.send(students);
+});
+
+
 
 
 
